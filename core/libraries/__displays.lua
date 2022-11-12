8ecc7c0af1e557c07a14a2eadc447053e394a199
eLiidyr <elidyrrr@gmail.com>
2022-11-12 03:37:04 -0600

    Update: Position() to displays Lib.

--- core/libraries/__displays.lua
+++ core/libraries/__displays.lua
@@ -7,8 +7,32 @@ function library:new(bp)
     local settings      = {pos={x=1, y=1}, bg={alpha=200, red=0, green=0, blue=0, visible=true}, flags={draggable=false, bold=true}, text={size=9, fonts={'Lucida Console','Arial'}, font='Lucida Console', alpha=255, red=245, green=200, blue=20, stroke={width=1, alpha=255, red=0, green=0, blue=0}}, padding=1}
 
     -- Public Methods.
-    self.new = function()
-        return bp.texts.new(settings)
+    self.new = function(s)
+        return bp.texts.new(s or settings)
+    end
+
+    self.position = function(display, layout, x, y)
+
+        if display and layout then
+            local x = tonumber(x) or layout.pos.x
+            local y = tonumber(y) or layout.pos.y
+
+            if x and y then
+                display:pos(x, y)
+
+                do -- Update the settings positions
+                    layout.pos.x = x
+                    layout.pos.y = y
+
+                end
+            
+            elseif bp and (not x or not y) then
+                --bp.helpers['popchat'].pop('PLEASE ENTER AN "X" OR "Y" COORDINATE!')
+
+            end
+
+        end
+
     end
 
     self.center = function(obj)
