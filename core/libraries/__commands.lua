a08e035d74c72b9b6f16b4f7f2ed6dd20abff951
eLiidyr <elidyrrr@gmail.com>
2022-11-21 10:44:01 -0600

    Update: Commands moved to functions.

--- core/libraries/__commands.lua
+++ core/libraries/__commands.lua
@@ -1,12 +1,59 @@
 local library = {}
 function library:new(bp)
     local bp = bp
+    local pm = {}
 
     -- Private Variables.
 
     -- Public Variables.
 
     -- Private Methods.
+    pm['toggle'] = function()
+        bp.enabled = bp.enabled ~= true and true or false
+        bp.helpers.popchat.pop(string.format('BUDDYPAL AUTOMATION ENABLED: \\cs(%s)%s\\cr', bp.colors.setting, tostring(bp.enabled):upper()))
+
+        if not bp.enabled then
+            bp.libs.__queue.clear()
+        end
+
+    end
+
+    pm['on'] = function()
+        bp.enabled = true
+        bp.helpers.popchat.pop(string.format('BUDDYPAL AUTOMATION ENABLED: \\cs(%s)%s\\cr', bp.colors.setting, tostring(bp.enabled):upper()))
+
+    end
+
+    pm['off'] = function()
+        bp.enabled = false
+        bp.libs.__queue.clear()
+        bp.helpers.popchat.pop(string.format('BUDDYPAL AUTOMATION ENABLED: \\cs(%s)%s\\cr', bp.colors.setting, tostring(bp.enabled):upper()))
+
+    end
+
+    pm['follow'] = function()
+        bp.libs.__orders.deliver('p*', ('bp follow %s'):format(bp.player.name))
+    end
+
+    pm['request_stop'] = function()
+        orders.deliver('p*', 'bp stop')
+    end
+
+    pm['stop'] = function()
+        bp.libs.__actions.stop()
+    end
+
+    pm['info'] = function()
+        local target = windower.ffxi.get_mob_by_target('t') or false
+
+        if target then
+            print(string.format('ID: %s | Index: %s | Claim ID: %s | POS(x=%s, y=%s, z=%s) | Zone: %s [ %s ]', target.id, target.index, target.claim_id, target.x, target.y, target.z, self.info.zone, self.res.zones[self.info.zone].en))
+            --table.vprint(target)
+            --table.vprint(windower.ffxi.get_player())
+            --table.vprint(self.me)
+        end
+
+    end
 
     -- Public Methods.
 
@@ -19,41 +66,8 @@ function library:new(bp)
             local popchat = bp.helpers.popchat
             local orders = bp.libs.__orders
 
-            if command == 'toggle' then
-                bp.enabled = bp.enabled ~= true and true or false
-
-                if not bp.enabled then
-                    --self.helpers['queue'].clear() ##QUEUE!
-                end
-                popchat.pop(string.format('BUDDYPAL AUTOMATION ENABLED: %s.', tostring(bp.enabled)))
-
-            elseif command == 'on' then
-                bp.enabled = true
-                popchat.pop('BUDDYPAL AUTOMATION NOW ENABLED!')
-
-            elseif command == 'off' then
-                bp.enabled = false
-                popchat.pop('BUDDYPAL AUTOMATION NOW DISABLED!')
-
-            elseif command == 'follow' then
-                orders.deliver('p*', ('bp follow %s'):format(bp.player.name))
-
-            elseif command == 'stop' then
-                bp.libs.__actions.stop()
-
-            elseif command == 'request_stop' then
-                orders.deliver('p*', 'bp stop')
-
-            elseif command == 'info' then
-                local target = windower.ffxi.get_mob_by_target('t') or false
-
-                if target then
-                    print(string.format('ID: %s | Index: %s | Claim ID: %s | POS(x=%s, y=%s, z=%s) | Zone: %s [ %s ]', target.id, target.index, target.claim_id, target.x, target.y, target.z, self.info.zone, self.res.zones[self.info.zone].en))
-                    --table.vprint(target)
-                    --table.vprint(windower.ffxi.get_player())
-                    --table.vprint(self.me)
-                end
-                --print(table.concat(self.player.buffs, '\n'))
+            if pm[command] then
+                pm[command](commands)
 
             elseif S{'r','rld','reload'}:contains(command) then
                 windower.send_command('lua r bp4')
