5d31013177c297187997988493301dbbc654cf3c
eLiidyr <elidyrrr@gmail.com>
2022-12-12 12:54:00 -0600

    Version Update.

--- bp4.lua
+++ bp4.lua
@@ -1,6 +1,6 @@
 _addon.name     = 'bp4'
 _addon.author   = 'Elidyr'
-_addon.version  = '4.20221130'
+_addon.version  = '4.20221212'
 _addon.command  = 'bp'
 --[[ From all your friends on Lakshmi, RIP Kanobrown, and you will be forever missed in the FFXI Community. ]]--
 
