730424e27378191b00ecef2b0843ab9733a47ede
eLiidyr <elidyrrr@gmail.com>
2023-01-09 00:53:13 -0600

    Update: Multiple logic adjustments.

--- core/libraries/__distance.lua
+++ core/libraries/__distance.lua
@@ -5,24 +5,30 @@ function library:new(bp)
     -- Public Methods.
     self.canMelee = function(target) return target and self.get(target) < (1.85 + target.model_size + bp.me.model_size) or false end
     self.get = function(target)
-        local m = windower.ffxi.get_mob_by_target('me') or false
-        local t = bp.libs.__target.get(target)
+        local t = (target and target.x and target.y and target.z) and target or bp.libs.__target.get(target)
 
-        if m and t and t.x and t.y and t.z then
-            return ((V{m.x, m.y, (m.z*-1)} - V{t.x, t.y, (t.z*-1)}):length())
+        if bp.me and t and t.x and t.y and t.z then
+            return ((V{bp.me.x, bp.me.y, (bp.me.z*-1)} - V{t.x, t.y, (t.z*-1)}):length())
         end
         return 0
 
     end
 
     self.pet = function(target)
-        local m = windower.ffxi.get_mob_by_target('pet') or false
-        local t = bp.libs.__target.get(target)
+        local t = (target and target.x and target.y and target.z) and target or bp.libs.__target.get(target)
 
-        if m and t then
-            return ((V{m.x, m.y, m.z} - V{t.x, t.y, t.z}):length())
+        if bp.me and t then
+            return ((V{bp.me.x, bp.me.y, (bp.me.z*-1)} - V{t.x, t.y, (t.z*-1)}):length())
         end
         return 0
+
+    end
+
+    self.height = function(target)
+
+        if bp.me and target and target.z then
+            return math.abs(target.z - bp.me.z)
+        end
 
     end
 
