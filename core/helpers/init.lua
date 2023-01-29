87e8632c31911aebca73567e976fe599b0ffd02b
eLiidyr <elidyrrr@gmail.com>
2023-01-29 13:52:51 -0600

    Add: Initialization Helper.

---
+++ core/helpers/init.lua
@@ -0,0 +1,68 @@
+local buildHelper = function(bp, hmt)
+    local bp        = bp
+    local helper    = setmetatable({events={}}, hmt)
+    local settings  = bp.__settings.new('init')
+
+    helper.new = function()
+        local new = setmetatable({events={}}, hmt)
+        local pvt = {}
+
+        -- Private Variables.
+        local default_aliases = {
+
+            -- RINGS.
+            {'wring',       "bp wring"},
+            {'dring',       "bp dring"},
+            {'hring',       "bp hring"},
+            {'mring',       "bp mring"},
+            {'wrings',      "bp ord p bp wring"},
+            {'drings',      "bp ord p bp dring"},
+            {'hrings',      "bp ord p bp hring"},
+            {'mrings',      "bp ord p bp mring"},
+    
+        }
+        
+        local default_binds = {
+
+            '@b bp toggle',
+            '@f bp follow',
+            '@s bp request_stop',
+            '@a bp assist',
+            '@p poke',
+            '@m bp ord p bp mount',
+            '@t bp target t',
+            '@p bp target pt',
+            '@, bp target lt',
+            '@. bp target et',
+            '@[ bp ord p bp on',
+            '@] bp ord p bp off',
+            '@/ bp mode',
+    
+        }
+
+        do -- Private Settings.
+            settings.accounts   = settings.accounts or T{}
+            settings.aliases    = settings.aliases or default_aliases
+            settings.binds      = settings.binds or default_binds
+
+        end
+
+        -- Save after all settings have been initialized.
+        settings:save()
+
+        -- All functions reegistered locally.
+        do
+            bp.accounts = T(settings.accounts)
+            bp.__keybinds.register(settings.binds)
+            bp.__alias.register(settings.aliases)
+
+        end
+
+        return new
+
+    end
+
+    return helper
+
+end
+return buildHelper
