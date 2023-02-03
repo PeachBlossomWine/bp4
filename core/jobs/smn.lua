be3986284b4c9f2c916664305bac4d73173a0908
eLiidyr <elidyrrr@gmail.com>
2023-02-02 19:02:30 -0600

    Add: SMN Core.

--- core/jobs/smn.lua
+++ core/jobs/smn.lua
@@ -57,28 +57,347 @@ function job:init(bp, settings, __getsub)
         if bp.player.status == 1 then
             local target = bp.core.target() or windower.ffxi.get_mob_by_target('t') or false
 
-            -- HATE GENERATION.
-            if settings.hate and settings.hate.enabled and (os.clock()-self.__timers.hate) >= settings.hate.delay and target then
+            if pet then 
 
-            end
+                if settings.ja and bp.core.canAct() then
 
-            if settings.ja and bp.core.canAct() then
+                    -- RELEASE.
+                    if settings.summon and settings.summon.enabled and pet.name ~= settings.summon.name and bp.core.ready("Release") then
+                        bp.core.add("Release", bp.player, bp.core.priority("Release"))
 
-            end
+                    -- AVATARS FAVOR.
+                    elseif bp.core.ready("Avatar's Favor", 431) then
+                        bp.core.add("Avatar's Favor", bp.player, bp.core.priority("Avatar's Favor"))
 
-            if settings.buffs then
+                    elseif pet.status == 0 and target then
 
-                if bp.core.canAct() then
+                        -- ASSAULT.
+                        if settings.assault and bp.core.ready("Assault") then
+                            bp.core.add("Assault", target, bp.core.priority("Assault"))
+                        end
+
+                    end
+
+                    -- BLOOD PACTS THAT PURELY DEAL DAMAGE & MEWING.
+                    if pet.status == 0 and target then
+
+                        -- ASTRAL FLOW.
+                        if settings['astral flow'] and bp.core.ready("Astral Flow", 55) and settings.bpr and settings.bpr.enabled and not settings.assault then
+                            bp.core.add("Astral Flow", bp.player, bp.core.priority("Astral Flow"))
+                        end
+
+                        -- USE APOGEE BEFORE ASTRAL CONDUIT.
+                        if (not settings.apogee or settings.apogee and not bp.core.ready("Apogee", 583)) then
+
+                            -- ASTRAL CONDUIT.
+                            if settings['astral conduit'] and bp.core.ready("Astral Conduit", 504) and (settings.bpr and settings.bpr.enabled and not settings.assault) or (settings.bpw and settings.bpw.enabled and not settings.assault and settings.bpw.pacts[pet.name] == "Mewing Lullaby") then
+                                bp.core.add("Astral Conduit", bp.player, bp.core.priority("Astral Conduit"))
+                            end
+
+                        end
+                        
+                        -- BLOOD PACT: RAGES.
+                        if settings.bpr and settings.bpr.enabled and not settings.assault then
+                            local rage = settings.bpr.pacts[pet.name] or false
+
+                            if rage and bp.core.ready(rage) then
+
+                                -- APOGEE.
+                                if settings.apogee and bp.core.ready("Apogee", 583) then
+                                    bp.core.add("Apogee", bp.player, bp.core.priority("Apogee"))
+
+                                -- MANA CEDE.
+                                elseif settings['mana cede'] and bp.core.ready("Mana Cede") then
+                                    bp.core.add("Mana Cede", bp.player, bp.core.priority("Mana Cede"))
+
+                                else
+                                    bp.core.add(rage, target, bp.core.priority(rage))
+                                    
+                                end
+
+                            end
+
+                        end
+
+                        -- MEWING LULLABY.
+                        if settings.bpw and settings.bpw.enabled and not settings.assault then
+                            local ward = settings.bpw.pacts[pet.name] or false
+
+                            if ward and ward == "Mewing Lullaby" and bp.core.ready(ward) then
+                                bp.core.add(ward, target, bp.core.priority(ward))
+                            end
+
+                        end
+
+                    elseif pet.status == 1 and settings.bpr and settings.bpr.enabled and target then
+
+                        -- ASTRAL FLOW.
+                        if settings['astral flow'] and bp.core.ready("Astral Flow", 55) and settings.bpr and settings.bpr.enabled then
+                            bp.core.add("Astral Flow", bp.player, bp.core.priority("Astral Flow"))
+                        end
+
+                        -- USE APOGEE BEFORE ASTRAL CONDUIT.
+                        if (not settings.apogee or settings.apogee and not bp.core.ready("Apogee", 583)) then
+
+                            -- ASTRAL CONDUIT.
+                            if settings['astral conduit'] and bp.core.ready("Astral Conduit", 504) and (settings.bpr and settings.bpr.enabled) or (settings.bpw and settings.bpw.enabled and settings.bpw.pacts[pet.name] == "Mewing Lullaby") then
+                                bp.core.add("Astral Conduit", bp.player, bp.core.priority("Astral Conduit"))
+                            end
+
+                        end
+                        
+                        -- BLOOD PACT: RAGES.
+                        if settings.bpr and settings.bpr.enabled then
+                            local rage = settings.bpr.pacts[pet.name] or false
+
+                            if rage and bp.core.ready(rage) then
+
+                                -- APOGEE.
+                                if settings.apogee and bp.core.ready("Apogee", 583) then
+                                    bp.core.add("Apogee", bp.player, bp.core.priority("Apogee"))
+
+                                -- MANA CEDE.
+                                elseif settings['mana cede'] and bp.core.ready("Mana Cede") then
+                                    bp.core.add("Mana Cede", bp.player, bp.core.priority("Mana Cede"))
+
+                                else
+                                    bp.core.add(rage, target, bp.core.priority(rage))
+
+                                end
+
+                            end
+
+                        end
+
+                        -- MEWING LULLABY.
+                        if settings.bpw and settings.bpw.enabled and not settings.assault then
+                            local ward = settings.bpw.pacts[pet.name] or false
+
+                            if ward and ward == "Mewing Lullaby" and bp.core.ready(ward) then
+                                bp.core.add(ward, target, bp.core.priority(ward))
+                            end
+
+                        end
+
+                    end
 
                 end
 
-                if bp.core.canCast() then
+                if settings.buffs then
 
+                    if bp.core.canAct() then
+
+                        -- BLOOD PACTS THAT GIVE A PLAYER OR PLAYERS A BUFF.
+                        if pet.status == 0 and not bp.core.buff(504) then
+
+                            -- BLOOD PACT: WARDS.
+                            if settings.bpw and settings.bpw.enabled and not settings.assault then
+                                local ward = settings.bpw.pacts[pet.name] or false
+
+                                if ward and bp.JA[ward] and (bp.JA[ward].status and bp.core.ready(ward, bp.JA[ward].status) or not bp.JA[ward].status) then
+                                    bp.core.add(ward, bp.player, bp.core.priority(ward))
+                                end
+
+                            end
+
+                        elseif pet.status == 1 and not bp.core.buff(504) then
+
+                            -- BLOOD PACT: WARDS.
+                            if settings.bpw and settings.bpw.enabled then
+                                local ward = settings.bpw.pacts[pet.name] or false
+
+                                if ward and bp.JA[ward] and (bp.JA[ward].status and bp.core.ready(ward, bp.JA[ward].status) or not bp.JA[ward].status) then
+                                    bp.core.add(ward, bp.player, bp.core.priority(ward))
+                                end
+
+                            end
+    
+                        end
+
+                    end
+
+                end
+
+            elseif not pet then
+
+                -- SUMMON.
+                if settings.summon and settings.summon.enabled and bp.core.ready(settings.summon.name) and bp.core.canCast() then
+                    bp.core.add(settings.summon.name, bp.player, bp.core.priority(settings.summon.name))
                 end
 
             end
 
         elseif bp.player.status == 0 then
+
+            if pet then 
+
+                if settings.ja and bp.core.canAct() then
+
+                    -- RELEASE.
+                    if settings.summon and settings.summon.enabled and pet.name ~= settings.summon.name and bp.core.ready("Release") then
+                        bp.core.add("Release", bp.player, bp.core.priority("Release"))
+
+                    -- AVATARS FAVOR.
+                    elseif bp.core.ready("Avatar's Favor", 431) then
+                        bp.core.add("Avatar's Favor", bp.player, bp.core.priority("Avatar's Favor"))
+
+                    elseif pet.status == 0 and target then
+
+                        -- ASSAULT.
+                        if settings.assault and bp.core.ready("Assault") then
+                            bp.core.add("Assault", target, bp.core.priority("Assault"))
+                        end
+
+                    end
+
+                    -- BLOOD PACTS THAT PURELY DEAL DAMAGE & MEWING.
+                    if pet.status == 0 and target then
+
+                        -- ASTRAL FLOW.
+                        if settings['astral flow'] and bp.core.ready("Astral Flow", 55) and settings.bpr and settings.bpr.enabled and not settings.assault then
+                            bp.core.add("Astral Flow", bp.player, bp.core.priority("Astral Flow"))
+                        end
+
+                        -- USE APOGEE BEFORE ASTRAL CONDUIT.
+                        if (not settings.apogee or settings.apogee and not bp.core.ready("Apogee", 583)) then
+
+                            -- ASTRAL CONDUIT.
+                            if settings['astral conduit'] and bp.core.ready("Astral Conduit", 504) and (settings.bpr and settings.bpr.enabled and not settings.assault) or (settings.bpw and settings.bpw.enabled and not settings.assault and settings.bpw.pacts[pet.name] == "Mewing Lullaby") then
+                                bp.core.add("Astral Conduit", bp.player, bp.core.priority("Astral Conduit"))
+                            end
+
+                        end
+                        
+                        -- BLOOD PACT: RAGES.
+                        if settings.bpr and settings.bpr.enabled and not settings.assault then
+                            local rage = settings.bpr.pacts[pet.name] or false
+
+                            if rage and bp.core.ready(rage) then
+
+                                -- APOGEE.
+                                if settings.apogee and bp.core.ready("Apogee", 583) then
+                                    bp.core.add("Apogee", bp.player, bp.core.priority("Apogee"))
+
+                                -- MANA CEDE.
+                                elseif settings['mana cede'] and bp.core.ready("Mana Cede") then
+                                    bp.core.add("Mana Cede", bp.player, bp.core.priority("Mana Cede"))
+
+                                else
+                                    bp.core.add(rage, target, bp.core.priority(rage))
+                                    
+                                end
+
+                            end
+
+                        end
+
+                        -- MEWING LULLABY.
+                        if settings.bpw and settings.bpw.enabled and not settings.assault then
+                            local ward = settings.bpw.pacts[pet.name] or false
+
+                            if ward and ward == "Mewing Lullaby" and bp.core.ready(ward) then
+                                bp.core.add(ward, target, bp.core.priority(ward))
+                            end
+
+                        end
+
+                    elseif pet.status == 1 and settings.bpr and settings.bpr.enabled and target then
+
+                        -- ASTRAL FLOW.
+                        if settings['astral flow'] and bp.core.ready("Astral Flow", 55) and settings.bpr and settings.bpr.enabled then
+                            bp.core.add("Astral Flow", bp.player, bp.core.priority("Astral Flow"))
+                        end
+
+                        -- USE APOGEE BEFORE ASTRAL CONDUIT.
+                        if (not settings.apogee or settings.apogee and not bp.core.ready("Apogee", 583)) then
+
+                            -- ASTRAL CONDUIT.
+                            if settings['astral conduit'] and bp.core.ready("Astral Conduit", 504) and (settings.bpr and settings.bpr.enabled) or (settings.bpw and settings.bpw.enabled and settings.bpw.pacts[pet.name] == "Mewing Lullaby") then
+                                bp.core.add("Astral Conduit", bp.player, bp.core.priority("Astral Conduit"))
+                            end
+
+                        end
+                        
+                        -- BLOOD PACT: RAGES.
+                        if settings.bpr and settings.bpr.enabled then
+                            local rage = settings.bpr.pacts[pet.name] or false
+
+                            if rage and bp.core.ready(rage) then
+
+                                -- APOGEE.
+                                if settings.apogee and bp.core.ready("Apogee", 583) then
+                                    bp.core.add("Apogee", bp.player, bp.core.priority("Apogee"))
+
+                                -- MANA CEDE.
+                                elseif settings['mana cede'] and bp.core.ready("Mana Cede") then
+                                    bp.core.add("Mana Cede", bp.player, bp.core.priority("Mana Cede"))
+
+                                else
+                                    bp.core.add(rage, target, bp.core.priority(rage))
+
+                                end
+
+                            end
+
+                        end
+
+                        -- MEWING LULLABY.
+                        if settings.bpw and settings.bpw.enabled and not settings.assault then
+                            local ward = settings.bpw.pacts[pet.name] or false
+
+                            if ward and ward == "Mewing Lullaby" and bp.core.ready(ward) then
+                                bp.core.add(ward, target, bp.core.priority(ward))
+                            end
+
+                        end
+
+                    end
+
+                end
+
+                if settings.buffs then
+
+                    if bp.core.canAct() then
+
+                        -- BLOOD PACTS THAT GIVE A PLAYER OR PLAYERS A BUFF.
+                        if pet.status == 0 and not bp.core.buff(504) then
+
+                            -- BLOOD PACT: WARDS.
+                            if settings.bpw and settings.bpw.enabled and not settings.assault then
+                                local ward = settings.bpw.pacts[pet.name] or false
+
+                                if ward and bp.JA[ward] and (bp.JA[ward].status and bp.core.ready(ward, bp.JA[ward].status) or not bp.JA[ward].status) then
+                                    bp.core.add(ward, bp.player, bp.core.priority(ward))
+                                end
+
+                            end
+
+                        elseif pet.status == 1 and not bp.core.buff(504) then
+
+                            -- BLOOD PACT: WARDS.
+                            if settings.bpw and settings.bpw.enabled then
+                                local ward = settings.bpw.pacts[pet.name] or false
+
+                                if ward and bp.JA[ward] and (bp.JA[ward].status and bp.core.ready(ward, bp.JA[ward].status) or not bp.JA[ward].status) then
+                                    bp.core.add(ward, bp.player, bp.core.priority(ward))
+                                end
+
+                            end
+    
+                        end
+
+                    end
+
+                end
+
+            elseif not pet then
+
+                -- SUMMON.
+                if settings.summon and settings.summon.enabled and bp.core.ready(settings.summon.name) and bp.core.canCast() then
+                    bp.core.add(settings.summon.name, bp.player, bp.core.priority(settings.summon.name))
+                end
+
+            end
 
         end
 
