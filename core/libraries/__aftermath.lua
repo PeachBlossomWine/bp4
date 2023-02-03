5be070408fcc200ef89b9f8706288f11c2ae0f5b
eLiidyr <elidyrrr@gmail.com>
2023-02-02 19:05:41 -0600

    Fix: Multiple bugs with AM logics.

--- core/libraries/__aftermath.lua
+++ core/libraries/__aftermath.lua
@@ -3,9 +3,10 @@ function library:new(bp)
     local bp = bp
 
     -- Private Variables.
-    local __levels     = {270,271,272,273}
-    local __relics     = {"Spharai","Mandau","Excalibur","Ragnarok","Guttler","Bravura","Apocalypse","Gungnir","Kikoku","Amanomurakumo","Mjollnir","Claustrum","Yoichinoyumi","Annihilator"}
-    local __aftermaths = {
+    local __level       = {270,271,272}
+    local __levels      = {270,271,272,273}
+    local __relics      = {"Spharai","Mandau","Excalibur","Ragnarok","Guttler","Bravura","Apocalypse","Gungnir","Kikoku","Amanomurakumo","Mjollnir","Claustrum","Yoichinoyumi","Annihilator"}
+    local __aftermaths  = {
         
         ["Verethragna"]         = "Victory Smite",
         ["Glanzfaust"]          = "Ascetic's Fury",
@@ -41,7 +42,7 @@ function library:new(bp)
         ["Heishi Shorinken"]    = "Blade: Shun",
         ["Masamune"]            = "Tachi: Fudo",
         ["Kogarasumaru"]        = "Tachi: Rana",
-        ["Dojikiri Yasutsuna"]  = "Tashi: Shoha",
+        ["Dojikiri Yasutsuna"]  = "Tachi: Shoha",
         ["Gambateinn"]          = "Dagan",
         ["Yagrush"]             = "Mystic Boon",
         ["Idris"]               = "Exudation",
@@ -75,28 +76,12 @@ function library:new(bp)
     }
 
     -- Public Methods.
-    self.weaponskill = function(weapon)
-        return weapon and __aftermaths[weapon] or false            
-    end
-
-    self.id = function(level)
-        
-        if self.checkRelic() then
-            return __levels[4]
-        
-        elseif level and __levels[level] then
-            return __levels[level]
-
-        end
-        return false
-        
-    end
-
+    self.weaponskill = function(weapon) return weapon and __aftermaths[weapon] or false end
     self.active = function()
         
         for id in T(bp.player.buffs):it() do
                     
-            if T(__levels):contains(v) then
+            if T(__levels):contains(id) then
                 return true
             end
             
@@ -105,8 +90,50 @@ function library:new(bp)
 
     end
 
-    self.checkRelic = function() -- UPDATE!
-        
+    self.canReplace = function()
+        if not bp.core.get('am') then return false end
+
+        for id in T(bp.player.buffs):it() do
+
+            if T(__levels):contains(id) then
+                local level = __level[id]
+
+                if level and bp.core.get('am').level > level then
+                    return true
+                end
+
+            end
+
+        end
+
+    end
+
+    self.getWeapon = function()
+
+        if T{'COR','RNG'}:contains(bp.player.main_job) then
+            return bp.__inventory.getByIndex(bp.__equipment.get(2).bag, bp.__equipment.get(2).index)
+
+        else
+            return bp.__inventory.getByIndex(bp.__equipment.get(0).bag, bp.__equipment.get(0).index)
+
+        end
+        return nil
+
+    end
+
+    self.getAvailable = function()
+        local available = {}
+
+        for slot in T{0,2}:it() do
+            local index, count, id, status, bag, resource = bp.__inventory.getByIndex(bp.__equipment.get(slot).bag, bp.__equipment.get(slot).index)
+
+            if index and resource and __aftermaths[resource.en] then
+                table.insert(available, __aftermaths[resource.en])
+            end
+
+        end
+        return available
+
     end
 
     return self
