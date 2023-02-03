4d369c85b2a8d52795997df353c67a7f1f57142d
eLiidyr <elidyrrr@gmail.com>
2023-02-02 19:12:44 -0600

    Fix: Pet distance not calculated properly.

--- core/libraries/__distance.lua
+++ core/libraries/__distance.lua
@@ -5,7 +5,7 @@ function library:new(bp)
     -- Public Methods.
     self.canMelee = function(target) return target and self.get(target) < (1.85 + target.model_size + bp.me.model_size) or false end
     self.get = function(target)
-        local t = (target and target.x and target.y and target.z) and target or bp.libs.__target.get(target)
+        local t = (target and target.x and target.y and target.z) and target or bp.__target.get(target)
 
         if bp.me and t and t.x and t.y and t.z then
             return ((V{bp.me.x, bp.me.y, (bp.me.z*-1)} - V{t.x, t.y, (t.z*-1)}):length())
@@ -15,12 +15,13 @@ function library:new(bp)
     end
 
     self.pet = function(target)
-        local t = (target and target.x and target.y and target.z) and target or bp.libs.__target.get(target)
+        local t = (target and target.x and target.y and target.z) and target or bp.__target.get(target)
+        local p = bp.__target.get('pet')
 
-        if bp.me and t then
-            return ((V{bp.me.x, bp.me.y, (bp.me.z*-1)} - V{t.x, t.y, (t.z*-1)}):length())
+        if p and t then
+            return ((V{p.x, p.y, (p.z*-1)} - V{t.x, t.y, (t.z*-1)}):length())
         end
-        return 0
+        return 999
 
     end
 
