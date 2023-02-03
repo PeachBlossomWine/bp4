8305af8ed1990fbd7c8f0a10d46fe0c69b509d2a
eLiidyr <elidyrrr@gmail.com>
2023-02-02 19:07:47 -0600

    Add: Subjob support and hasHateSpells().

--- core/libraries/__blu.lua
+++ core/libraries/__blu.lua
@@ -5,7 +5,19 @@ function library:new(bp)
 
     -- Public Methods.
     self.getSpellSet = function()
-        return bp and bp.player and bp.player.main_job_id == 16 and T(windower.ffxi.get_mjob_data().spells):filter(function(id) return id ~= 512 end) or T{}
+
+        if bp and bp.player and bp.player.main_job_id == 16 then
+            return T(windower.ffxi.get_mjob_data().spells):filter(function(id) return id ~= 512 end) or T{}
+        
+        elseif bp and bp.player and bp.player.sub_job_id == 16 then
+            return T(windower.ffxi.get_sjob_data().spells):filter(function(id) return id ~= 512 end) or T{}
+
+        end
+
+    end
+
+    self.hasHateSpells = function(spells)
+        return self.getSpellSet():filter(function(id) return bp.res.spells[id] and T(spells):contains(bp.res.spells[id].en) end)        
     end
 
     return self
