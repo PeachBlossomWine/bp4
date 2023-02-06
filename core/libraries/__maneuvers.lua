b9a07cdc742655a997ceab34b31f2ea4a2f8430e
eLiidyr <elidyrrr@gmail.com>
2023-02-06 12:39:34 -0600

    Fix: Not working if already buffed.

--- core/libraries/__maneuvers.lua
+++ core/libraries/__maneuvers.lua
@@ -4,24 +4,25 @@ function library:new(bp)
     local pm = {}
 
     -- Public Methods.
-    self.active = function() return T(bp.player.buffs):map(function(id) return T{300,301,302,303,304,305,306,307}:contains(id) and bp.res.buffs[id].en or nil end) end
-    self.getMissing = function(maneuvers)
+    self.active = function() return T(T(bp.player.buffs):map(function(id) return T{300,301,302,303,304,305,306,307}:contains(id) and bp.res.buffs[id].en or nil end)) end
+    self.getMissing = function()
         local current = self.active()
-
-        if maneuvers and #maneuvers > 0 then
-
-            for ci=1, #current do
-                                    
+        
+        if bp and bp.core and bp.core.get('maneuvers') and #bp.core.get('maneuvers').list > 0 then
+            local maneuvers = T(bp.core.get('maneuvers').list):copy()
+            
+            for maneuver in current:it() do
+                
                 for mi=1, 3 do
 
-                    if maneuvers[mi] == current[ci] then
+                    if maneuvers[mi] == maneuver then
                         table.remove(maneuvers, mi)
                     end
 
                 end
 
             end
-            return maneuvers
+            return T(maneuvers)
 
         end
         return T{}
