4f7f3dcc540cfea73e16ea63e7068191093906ca
eLiidyr <elidyrrr@gmail.com>
2023-02-25 14:36:48 -0600

    Version Update.

--- bp4.lua
+++ bp4.lua
@@ -1,6 +1,6 @@
 _addon.name     = 'bp4'
 _addon.author   = 'Elidyr'
-_addon.version  = '4.20230218'
+_addon.version  = '4.20230225'
 _addon.command  = 'bp'
 --[[ From all your friends on Lakshmi, RIP Kanobrown, and you will be forever missed in the FFXI Community. ]]--
 local bp = nil
@@ -12,4 +12,5 @@ end)
 
 if windower.ffxi.get_info() and windower.ffxi.get_info().logged_in then
     bp = require('/core/bootstrap')
+
 end
